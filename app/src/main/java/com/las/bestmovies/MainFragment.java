c3d932cda1f2d650359990e22c8c77ef1394a4b6
Aleksey <melle095@gmail.com>
2016-09-08 00:26:27 +0300

    Base grid layout

---
+++ app/src/main/java/com/las/bestmovies/MainFragment.java
@@ -0,0 +1,47 @@
+package com.las.bestmovies;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.GridView;
+import android.widget.Toast;
+
+
+public class MainFragment extends Fragment {
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+
+        final View rootView = inflater.inflate(R.layout.fragment_blank, container, false);
+        GridView gridView = (GridView) rootView.findViewById(R.id.gridview);
+        ImageAdapter mImageAdapter = new ImageAdapter(rootView.getContext());
+        gridView.setAdapter(mImageAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                Toast.makeText(getActivity(), "" + position,
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+
+
+}
